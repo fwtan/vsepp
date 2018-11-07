c3ac5f83f2e2d0edd0acd60adc781cf472181e38
FUWEN TAN <fuwen.tan@gmail.com>
2018-11-06 22:49:47 -0500

    full

--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 from vocab import Vocabulary
 import evaluation
-evaluation.evalrank("data/runs/coco_vse++/model_best.pth.tar", data_path="data/data", split="test")
+evaluation.evalrank("data/runs/coco_vse++_vggfull_restval_finetune/model_best.pth.tar", data_path="data/data", split="test")
